9c4c7dda4ddf11dd89322eed60d230e4ae23acc4
bsergi <bsergi@nlr.gov>
2026-05-20 11:21:46 -0400

    Remove MGA RV stuff for now

--- reeds/input_processing/mcs_sampler.py
+++ reeds/input_processing/mcs_sampler.py
@@ -1933,7 +1933,6 @@ if __name__ == '__main__' and not hasattr(sys, 'ps1'):
     parser.add_argument('reeds_path', help='ReEDS directory')
     parser.add_argument('inputs_case', help='Output directory')
     parser.add_argument('--nolog', '-n', default=False, action='store_true', help='turn off logging for debugging')
-    parser.add_argument('--random_vector', '-r', default=False, action='store_true', help='compute random vector for MGA')
                     
     args = parser.parse_args()
     reeds_path = os.path.abspath(args.reeds_path)
@@ -1955,21 +1954,16 @@ if __name__ == '__main__' and not hasattr(sys, 'ps1'):
             logpath=os.path.join(os.path.dirname(inputs_case), 'gamslog.txt'),
         )
     
-    # use sampling capability to set up random vector for modeing to generate alternatives (MGA)
-    if random_vector:
-        print("MGA random vector sampling will be implemented in the future")
-    # set up monte carlo sampling (MCS)
-    else:
-        # Read switches and check if MCS_runs is enabled.
-        sw = reeds.io.get_switches(inputs_case)
-        MCS_runs = int(sw.get('MCS_runs', 0))
-        MCS_lhs = int(sw.get('MCS_lhs', 0))
+    # Read switches and check if MCS_runs is enabled.
+    sw = reeds.io.get_switches(inputs_case)
+    MCS_runs = int(sw.get('MCS_runs', 0))
+    MCS_lhs = int(sw.get('MCS_lhs', 0))
 
-        if MCS_runs >= 1:
-            print('Starting mcs_sampler.py')
-            main(reeds_path, inputs_case, n_samples=MCS_runs, lhs_sampling=MCS_lhs)
-        else:
-            print('MCS_runs switch is set to 0 or not found. No Monte Carlo sampling will be performed')
+    if MCS_runs >= 1:
+        print('Starting mcs_sampler.py')
+        main(reeds_path, inputs_case, n_samples=MCS_runs, lhs_sampling=MCS_lhs)
+    else:
+        print('MCS_runs switch is set to 0 or not found. No Monte Carlo sampling will be performed')
 
     # Final log/timing update.
     reeds.log.toc(
