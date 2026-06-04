0bd6e10f80e5b01fdcc390288ae121d441d88948
Mindermann, Kennedy <Kennedy.Mindermann@nrel.gov>
2026-06-04 15:37:19 -0600

    remove unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,7 @@ import datetime
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-import reeds
 from reeds import generate_markdown
-
 
 # Configuration file for the Sphinx documentation builder.
 #
